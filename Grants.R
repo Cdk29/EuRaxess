470ed1a3f8e9fe1b8bcbea120773e76a723acaf3
Etienne <etienne.rolland.cdk@gmail.com>
2020-03-11 10:41:33 +0100

    Fun to scrap grants from EuRaxess

--- Grants.R
+++ Grants.R
@@ -1,7 +1,6 @@
 
 #block-system-main a
 #
-search_url<-"https://euraxess.ec.europa.eu/funding/search/"
 scrape_urls_grant <- function(search_url) {
   #function to scrape all the urls of grant from the result table
   search_space <- read_html(search_url)
@@ -14,7 +13,9 @@ scrape_urls_grant <- function(search_url) {
   return(urls)
 }
 
-scrape_urls_grant(search_url)
+# search_url<-"https://euraxess.ec.europa.eu/funding/search/"
+# 
+# scrape_urls_grant(search_url)
 
 
 scrape_urls_grants_euraxess <- function(search_url, last_page) {
@@ -27,13 +28,13 @@ scrape_urls_grants_euraxess <- function(search_url, last_page) {
   urls<-unlist(urls)
   #not forget the first page
   Sys.sleep(3)
-  urls<-c(urls, scrape_urls_grant (search_url))
+  urls<-c(urls, scrape_urls_grant(search_url))
   urls<-paste0("https://euraxess.ec.europa.eu", urls)
   return(urls)
 }
 
 
-read_job_offer <- function(url) {
+read_grant_offer <- function(url) {
   #job_offer <- read_html(url)
   job_offer <- tryCatch(read_html(url), error=function(e) NULL)
   if (is.null(job_offer)) {
@@ -46,29 +47,40 @@ read_job_offer <- function(url) {
 
   #all general text, scrap too much things but it is the only one that generalize for each page
   #job_offer %>% html_nodes("#node-498418 p")  %>%  html_text()
-  text_job <- job_offer %>% html_nodes( paste0("#node-", job_id, " p")) %>%
-    html_text() %>% paste( collapse = '')
-
-  job_offer %>% html_nodes("*") %>% html_text()
+  text_grant <- job_offer %>% html_nodes("p") %>%
+    html_text() %>% paste( collapse = ' ')
 
   #all the tickle box of the job offer :
-  tickle_boxes <- job_offer %>% html_nodes(".field-body ul") %>%  html_text() %>% paste(collapse = '')
+  tickle_boxes <- "Nothing."
 
   #location :
   #location <- job_offer %>% html_nodes(".field-country div") %>%  html_text()
-  institute <- job_offer %>% html_nodes(".field-company-institute div") %>%  html_text()
+  institute <- job_offer %>% html_nodes(".field-org-name div") %>%  html_text()
 
   #return a dataframe on each iteration of the apply, similar to page 278 of Text Mining in practice with R
   #rbindlist will be called after
 
   #part to create the dataframe
-  dt<-data.table(url, text_job, tickle_boxes, institute)
+  dt<-data.table(url, text_grant, tickle_boxes, institute)
   return(dt)
 }
 
 url<-"https://euraxess.ec.europa.eu/jobs/funding/phd-mathematics-design-experiments-regression-models-correlated-observations"
 
-read_job_offer(url)
+read_grant_offer(url)
+
+
+search_url<-"https://euraxess.ec.europa.eu/funding/search/"
+urls<-scrape_urls_grants_euraxess(search_url, 1) 
+
+
+grant_offers<-pblapply(urls, read_grant_offer)
+grant_offers<-rbindlist(grant_offers, use.names = TRUE )
+
+saveRDS(job_offer, file="grant_offers.RDS")
+
+
+
 
 
 

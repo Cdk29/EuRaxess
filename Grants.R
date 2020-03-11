322757f0611147173d756acddb5c37cbf242a99c
Etienne <etienne.rolland.cdk@gmail.com>
2020-03-11 11:34:42 +0100

    Functions to summarize all the grants offers

--- Grants.R
+++ Grants.R
@@ -80,7 +80,48 @@ grant_offers<-rbindlist(grant_offers, use.names = TRUE )
 saveRDS(grant_offers, file="grant_offers.RDS")
 
 
+summarise_all_grants_offers <- function(grant_offers, tagger, key_words) {
+  pb = txtProgressBar(min = 1, max = dim(grant_offers)[1], initial = 1)
+  grant_offers$summary<-"void"
+  for (i in 1:dim(grant_offers)[1]) {
+    setTxtProgressBar(pb,i)
+    summary<-resume_grant_offer(grant_offers[i,], tagger, key_words)
+    grant_offers[i,]$summary<-summary
+  }
+  return(grant_offers)
+}
+
+resume_grant_offer <- function(df, tagger, key_words) {
+  #shorten the job offer for latter manual review
+  #since key words are not calculated on the fly, we do not need to do the full annotation or parsing,
+  #just the sentences :
+  # joboffer <- udpipe_annotate(tagger, paste(df$text_job, df$tickle_boxes))
+  #update for grants :
+  joboffer <- udpipe_annotate(tagger, df$text_grant)
+  joboffer <- as.data.frame(joboffer)
+  keyw <- textrank_keywords(joboffer$lemma,
+                            relevant = joboffer$upos %in% c("NOUN", "ADJ"))
+  
+  joboffer$textrank_id <- unique_identifier(joboffer, c("sentence_id"))
+  sentences <- unique(joboffer[, c("textrank_id", "sentence")])
+  
+  #here the changes regarding the tutorial of text rank
+  terminology <- subset(joboffer, upos %in% c("NOUN", "ADJ"))
+  terminology <- terminology[, c("textrank_id", "lemma")]
+  terminology <- terminology[terminology$lemma %in% key_words,]
+  if (dim(terminology)[1]==0) {
+    s<-"EMPTY"
+    return(s)
+  }
+  tr <- textrank_sentences(data = sentences, terminology = terminology)
+  s <- summary(tr, n = 3, keep.sentence.order = TRUE)
+  s <- paste(s, collapse = " ")
+  return(s)
+}
+
+key_words<-c("systematic", "review", "text", "mining", "machine", "learning", "biology", 
+             "medecine", "medical", "natural", "processing", "language")
 
 
-
-
+grant_offers_summarized<-summarise_all_grants_offers(grant_offers, tagger, key_words)
+saveRDS(grant_offers_summarized, file="grant_offers_summarized.RDS")
